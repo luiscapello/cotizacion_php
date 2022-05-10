53857ec9ed671b2297c0f0471cf1f4d5e21b42ac
luiscapello <capello3005@gmail.com>
2022-05-10 12:02:28 -0500

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'c42c5df10b1431388204ff77e8d0622d50d379c6',
+        'name' => 'juandelatorre/up-task_mvc',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'juandelatorre/up-task_mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'c42c5df10b1431388204ff77e8d0622d50d379c6',
+            'dev_requirement' => false,
+        ),
+    ),
+);
